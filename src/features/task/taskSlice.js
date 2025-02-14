0ff2903890c5cc45773ecbb0d0da8d7e0406d177
nitish kumar <kushwaha30nitish2002@gmail.com>
2025-02-14 17:57:13 +0530

    fix:changes

--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -20,7 +20,7 @@ const getAuthHeaders = () => {
 export const getTasks = createAsyncThunk("api/tasks", async (_, thunkAPI) => {
   try {
     const response = await axios.get(API_URL, getAuthHeaders());
-    return response.data;
+    return response.data?.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.response?.data || "Failed to fetch tasks");
   }
